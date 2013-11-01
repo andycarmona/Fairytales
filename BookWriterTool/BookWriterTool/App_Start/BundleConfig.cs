7c2ae62aa8d12d09809182a0d91f6fcaaf5476d9
andresc <andres.carmona@condesign.se>
2013-11-01 07:01:34 +0100

    Fixed conflicts after changed theme and between jquery ui and bootstrap

--- BookWriterTool/BookWriterTool/App_Start/BundleConfig.cs
+++ BookWriterTool/BookWriterTool/App_Start/BundleConfig.cs
@@ -27,18 +27,31 @@ namespace BookWriterTool
             bundles.Add(new StyleBundle("~/Content/customcss").Include("~/Content/Book.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+                  "~/Content/themes/base/jquery.ui.core.css",
+                  "~/Content/themes/base/jquery.ui.resizable.css",
+                  "~/Content/themes/base/jquery.ui.selectable.css",
+                  "~/Content/themes/base/jquery.ui.accordion.css",
+                  "~/Content/themes/base/jquery.ui.autocomplete.css",
+                  "~/Content/themes/base/jquery.ui.button.css",
+                  "~/Content/themes/base/jquery.ui.dialog.css",
+                  "~/Content/themes/base/jquery.ui.slider.css",
+                  "~/Content/themes/base/jquery.ui.tabs.css",
+                  "~/Content/themes/base/jquery.ui.datepicker.css",
+                  "~/Content/themes/base/jquery.ui.progressbar.css",
+                  "~/Content/themes/base/jquery.ui.theme.css"));
+            bundles.Add(new StyleBundle("~/Content/themes/sunny/css").Include(
+                 "~/Content/themes/sunny/jquery.ui.core.css",
+                 "~/Content/themes/sunny/jquery.ui.resizable.css",
+                 "~/Content/themes/sunny/jquery.ui.selectable.css",
+                 "~/Content/themes/sunny/jquery.ui.accordion.css",
+                 "~/Content/themes/sunny/jquery.ui.autocomplete.css",
+                 "~/Content/themes/sunny/jquery.ui.button.css",
+                 "~/Content/themes/sunny/jquery.ui.dialog.css",
+                 "~/Content/themes/sunny/jquery.ui.slider.css",
+                 "~/Content/themes/sunny/jquery.ui.tabs.css",
+                 "~/Content/themes/sunny/jquery.ui.datepicker.css",
+                 "~/Content/themes/sunny/jquery.ui.progressbar.css",
+                 "~/Content/themes/sunny/jquery.ui.theme.css"));
         }
     }
 }
