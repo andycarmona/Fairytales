8fa417f1b431251d4c3575c9c27d9d022b6b12fe
andycarmona <andres.carmona@uni-app.se>
2015-04-23 15:20:06 +0200

    Migration to apply turn.js to book view

--- BookWriterTool/BookWriterTool/Controllers/HomeController.cs
+++ BookWriterTool/BookWriterTool/Controllers/HomeController.cs
@@ -1,43 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace BookWriterTool.Controllers
+﻿namespace BookWriterTool.Controllers
 {
-    using System.Globalization;
-    using System.Reflection;
-    using System.Resources;
-    using System.Threading;
-    using System.Web.Routing;
-
-    using BookWriterTool.Helpers;
+    using System.Web.Mvc;
     using BookWriterTool.Models;
     using BookWriterTool.Repositories;
 
     public class HomeController : Controller
     {
-
-        //private Localization localizationHandler;
-
         public HomeController()
             : this(new BookRepository())
         {
-
         }
+
         public HomeController(IBookRepository bookRepository)
         {
-    
-           
-
         }
-        [Authorize]
+
         public ActionResult Index(string user)
         {
-          //  this.ViewBag.message = this.localizationHandler.Localize("ChooseOption");
-
-            //return this.View();
             return RedirectToAction("EditBook", "Book");
         }
     }
